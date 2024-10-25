d817eeee90c2ae5adfb19927d1f48fd0e6ac6e07
Syed <syedaseeka@hotmail.com>
2024-10-25 07:52:55 -0700

    Update data.py

--- data.py
+++ data.py
@@ -2,7 +2,7 @@
 class WebData:
     url = 'https://opensource-demo.orangehrmlive.com/web/index.php/auth/login'
     dashboard_url = 'https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index'
-    excel_file = 'C:\\Users\\Nadeem\\Desktop\\ST\\DDFT\\test_data.xlsx'
+    excel_file = 'C:\\Users\\Syed\\Desktop\\ST\\DDFT\\test_data.xlsx'
     sheet_number = 'Sheet1'
 
 
